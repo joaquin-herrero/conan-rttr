4cc61fb7d16d846c01dfcdf27e135a1f86d33dc8
Joaquin Herrero Herrero <jherrero.sax@gmail.com>
2019-02-17 16:51:08 +0100

    Added rttr test_package simple example.

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-#include "hello.h"
+#include "rttr/registration"
+
+using namespace rttr;
+
+enum class color
+{
+  red,
+  green,
+  blue
+};
+
+RTTR_REGISTRATION
+{
+  rttr::registration::enumeration<color>("color")
+  (
+    value("red", color::red),
+    value("blue", color::blue),
+    value("green", color::green)
+  );
+}
 
 int main() {
-    hello();
+  type t = type::get<color>();
+  
+  if( !t.is_valid() )
+    return 1;
+
+  if( t.get_name() != "color" )
+    return 1;
+
+  if( !t.is_enumeration() )
+    return 1;
+
+  auto e = t.get_enumeration();
+
+  if( e.get_values().size() != 3 )
+    return 1;
+
+  return 0;
 }
